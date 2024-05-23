49a47a83e03067300db8b35c2ca83b64713d326e
Christoph Fink <christoph.fink@christophfink.com>
2024-05-23 15:16:07 +0200

    init

---
+++ src/foobar/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+import jpype
+
+__version__ = "0.0.0"
+
+jpype.startJVM()
+print("adfasdfasdf")
---
+++ tests/test_foobar.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+
+class TestFooBar:
+    def test_foo_bar(self):
+        assert True
