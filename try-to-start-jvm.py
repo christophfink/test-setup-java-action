fa837767cecb9d96c367fdd57002d88eea9208b3
Christoph Fink <christoph.fink@christophfink.com>
2024-05-31 08:43:43 +0200

    ...

--- try-to-start-jvm.py
+++ try-to-start-jvm.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
 import jpype
+import jpype.imports
 
 if not jpype.isJVMStarted():
-    jpype.startJVM(classpath="r5-v7.0-r5py-all.jar")
+    jpype.startJVM()
     import java.lang  # noqa: F401
     print("successfully started JVM")
