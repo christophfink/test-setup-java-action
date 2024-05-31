e536aeb4eb50ca36118957e7e47dc0180aaf19ed
Christoph Fink <christoph.fink@christophfink.com>
2024-05-31 08:16:26 +0200

    use jar classpath

--- try-to-start-jvm.py
+++ try-to-start-jvm.py
@@ -3,4 +3,6 @@
 import jpype
 
 if not jpype.isJVMStarted():
-    jpype.startJVM()
+    jpype.startJVM(classpath="r5-v7.0-r5py-all.jar")
+    import java.lang  # noqa: F401
+    print("successfully started JVM")
